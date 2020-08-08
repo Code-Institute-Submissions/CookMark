996510aa81b771ba391cc3681c939dd34a09f9e9
karomei <58730948+karomei@users.noreply.github.com>
2020-08-08 17:22:14 +0200

    Set up env variables for MongoDB

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import os
 import ssl
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env
 
 app = Flask(__name__)
 
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
+MONGO_URI = os.environ.get('MONGO_URI')
+
+app.config["MONGO_DBNAME"] = MONGO_DBNAME
+app.config["MONGO_URI"] = MONGO_URI
+
+mongo = PyMongo(app)
+
 @app.route('/')
-@app.route('/<name>')
-def index(name):
-    return '<h1>Hello {}!</h1>'.format(name)
+@app.route('/get_collections')
+def get_collections():
+    return render_template("collections.html", collections=mongo.db.collections.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
