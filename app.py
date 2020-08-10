de6289023f844a5ec765b71d34d5eb038c317079
karomei <58730948+karomei@users.noreply.github.com>
2020-08-10 21:08:26 +0200

    Change from email to username for register in app.py

--- app.py
+++ app.py
@@ -20,21 +20,21 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-  if 'email' in session:
-    return 'Welcome' + session['email']
+    if 'username' in session:
+        return 'Welcome ' + session['username']
 
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['email']})
+        existing_user = users.find_one({'name' : request.form['username']})
 
         if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['email'], 'password' : hashpass})
-            session['email'] = request.form['username']
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
             return redirect(url_for('index'))
         
         return 'That username already exists!'
