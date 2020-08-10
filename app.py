5c12ce8995294e1d7da5593271d7b29df85341d4
karomei <58730948+karomei@users.noreply.github.com>
2020-08-10 15:23:42 +0200

    Add register route and definition

--- app.py
+++ app.py
@@ -20,10 +20,26 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-  if 'username' in session:
-    return 'Welcome' + session['username']
+  if 'email' in session:
+    return 'Welcome' + session['email']
 
   return render_template('index.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['email']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['email'], 'password' : hashpass})
+            session['email'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
 
 @app.route('/get_collections')
 def get_collections():
