0a86f88ab72ca13a56ba8419d8d583072710273a
karomei <58730948+karomei@users.noreply.github.com>
2020-08-10 13:58:49 +0200

    Import bcrypt

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import ssl
 from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import bcrypt
 from os import path
 if path.exists("env.py"):
   import env
