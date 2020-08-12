f5d2dc33d30b6511adddaba46ec959796e107453
karomei <58730948+karomei@users.noreply.github.com>
2020-08-12 18:08:28 +0200

    Fix browser cache static issue

--- app.py
+++ app.py
@@ -31,7 +31,20 @@ def get_collections():
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
-    
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+        
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=5000,
