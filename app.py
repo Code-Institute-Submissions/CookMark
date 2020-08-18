4eefaedc4826c8337d269f21045b0e0bab1bfe17
karomei <58730948+karomei@users.noreply.github.com>
2020-08-18 19:55:25 +0200

    Remove overriding url function as not needed

--- app.py
+++ app.py
@@ -63,20 +63,6 @@ def delete_recipe(recipe_id):
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     return redirect(url_for('index'))
 
-# Add st_mtime to the url, overriding the old one and fixing url issue for static files.
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                 endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
-
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=5000,
